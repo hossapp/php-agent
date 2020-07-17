d66c3694984d885508747490b39197de43b6fd72
Trung Vu <trung@apitracker.com>
2020-07-17 00:59:24 -0700

    Minor clean up

--- examples/curl/file1.php
+++ examples/curl/file1.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../../src/Hoss/Agent.php';
 
 use Hoss\Agent;
 
-Agent::instrument('staging-Ad7N9vyP3ADAJ8VCVM3eHw7c2XuoJNujBEKe5gPLxWVd');
+Agent::instrument('HOSS API KEY');
 
 require __DIR__.'/file2.php';
 
